3615e2842f0d15191d5e879c3a11cd490fed2035
Herilala R <herilalah.rakotonasolo@gmail.com>
2023-05-03 16:23:13 +0300

    Initializing form component in Movement of Informatique

--- src/views/FluxInformatique/Movement.vue
+++ src/views/FluxInformatique/Movement.vue
@@ -27,17 +27,16 @@
           <v-switch
             class="text-center"
             color="primary"
-            label="stock"
-            value="stockChecked"
-            hide-details
-          ></v-switch>
+            label="Stock"
+            v-model="isEnabled"
+          />
         </v-col>
       </v-row>
-      <v-row justify-md="center">
+      <v-row justify-md="center"  class="pa-0 ma-0">
         <v-col cols="12" md="6">
           <v-text-field
           color="primary"
-          label="Article"
+          :label="isEnabled ? 'combo Article' : 'Article  '"
           variant="underlined"
           ></v-text-field>
         </v-col>
@@ -57,7 +56,28 @@
           >
           </v-autocomplete>
         </v-col>
+
+        <v-col cols="12" md="2">
+          <v-text-field
+          color="primary"
+          label="Qté demandé"
+          type="number"
+          variant="underlined"
+          ></v-text-field>
+        </v-col>
+        <v-col align-self="end">
+          <v-radio-group inline>
+              <v-radio label="KG" value="KG" color="primary"></v-radio>
+              <v-radio label="PIECE" value="PIECE" color="primary"></v-radio>
+              <v-radio label="METRE" value="METRE" color="primary"></v-radio>
+          </v-radio-group>
+        </v-col>
       </v-row>
     </v-form>
   </div>
 </template>
+<script setup>
+import { ref } from "vue";
+
+const isEnabled = ref(true);
+</script>
