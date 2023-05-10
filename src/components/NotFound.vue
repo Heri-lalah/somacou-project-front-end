f139a035630c34358b75aed389e46aed58e2cb33
Herilala R <herilalah.rakotonasolo@gmail.com>
2023-05-10 15:54:47 +0300

    page not found created

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
-    404 - Page not found
-  </div>
+    <div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+    <div class="pb-8">
+    <div class="text-center pt-8">
+    <h1 class="text-h1 font-weight-black">404</h1>
+    <h1 class="font-weight-medium py-8">oops! Page introuvable</h1>
+    <p>Oops! La page que vous recherchez n'existe pas. Il a peut-être été déplacé ou supprimé.</p>
+    </div>
+    </div>
+    </div>
+    </div>
 </template>
+
+<style type="text/css">
+      .text-9xl{
+      font-size: 14rem;
+      }
+      @media(max-width: 645px){
+      .text-9xl{
+      font-size: 5.75rem;
+      }
+      .text-6xl{
+      font-size: 1.75rem;
+      }
+      .text-2xl {
+      font-size: 1rem;
+      line-height: 1.2rem;
+      }
+      .py-8 {
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      }
+      .px-6 {
+      padding-left: 1.2rem;
+      padding-right: 1.2rem;
+      }
+      .mr-6{
+      margin-right: 0.5rem;
+      }
+      .px-12 {
+      padding-left: 1rem;
+      padding-right: 1rem;
+      }
+      }
+    </style>
